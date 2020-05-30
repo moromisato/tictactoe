545688d891f5d91f61bd23b963e10875fb2043c4
Luis Moromisato <moromisato.luis@gmail.com>
2020-05-30 19:03:10 -0300

    added color specific marker

--- src/components/Position.js
+++ src/components/Position.js
@@ -1,29 +1,41 @@
-import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
+import React from 'react';
+import { Text, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
 
 const height = Dimensions.get('screen').height
 
 export default function Position({value, onClick}) {
+
+  const xMarkerColor = {
+    color: '#1564be'
+  }
+
+  const oMakerColor = {
+    color: '#f56476'
+  }
+
   return (
     <TouchableOpacity style={styles.container} onPress={onClick}>
-        <Text style={styles.marker}>{value}</Text>
+        <Text style={value === 'X' ? [styles.marker, xMarkerColor] : [styles.marker, oMakerColor]}>{value}</Text>
     </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
     container:{
-        width: height * 0.15,
-        height: height * 0.15,
+        width: height * 0.12,
+        height: height * 0.12,
         margin: 10,
         borderRadius: height * 0.01,
-        backgroundColor: '#1564be',
+        borderWidth: 0,
+        backgroundColor: '#dae3e5',
+        //backgroundColor: '#1564be',
         justifyContent: 'center',
         alignItems: 'center',
     },
     marker:{
-      fontSize: height * 0.1,
+      fontFamily: '',
+      fontSize: height * 0.09,
       fontWeight: 'bold',
-      color: '#f8bbd0'
+
     }
 })
