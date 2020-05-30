b9bdf74416ca17ce07657fad2e0e84acf8ad7b47
Luis Moromisato <moromisato.luis@gmail.com>
2020-05-30 20:40:06 -0300

    changed app name and package name

--- android/app/src/main/java/com/jogodavelha/MainActivity.java
+++ android/app/src/main/java/com/jogodavelha/MainActivity.java
@@ -1,4 +1,4 @@
-package com.tictactoe;
+package com.jogodavelha;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "TicTacToe";
+    return "JogodaVelha";
   }
 }
