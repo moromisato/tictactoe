76dd939aa5f16a7f9ba9f1a070e7a3354749c05e
Luis Moromisato <moromisato.luis@gmail.com>
2020-11-15 12:24:05 -0300

    added theme provider to app.js

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import MainPage from './src/pages/MainPage'
+import ThemeProvider from './src/context/ThemeProvider'
 import './src/components/i18n/i18n'
 
 export default function App() {
-  return <MainPage />
+  return (
+    <ThemeProvider>
+      <MainPage />
+    </ThemeProvider>
+  )
 };
